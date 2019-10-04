273a8b228cda2d5faf4822da83e49ad88d85f719
Ventrae <ventrae@gmail.com>
2019-10-04 20:00:31 +0200

    Swapping back to SCSS, implementing simple navbar and footer with little styling.

---
+++ src/pages/Instrukcja.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+
+    <div class="col-6 mx-auto alert alert-danger alert-dismissible fade show" role="alert">
+      Strona "Instrukcja" narazie nie istnieje!
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Instrukcja"
+    }
+</script>
+
+<style scoped>
+
+</style>
